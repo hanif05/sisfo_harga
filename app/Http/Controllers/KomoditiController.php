7da5a4acf114f53ab29b2ca644a3066293290b9c
hanif05 <yukizumarushinzuru@gmail.com>
2019-05-30 16:07:11 +0700

    first commit

---
+++ app/Http/Controllers/KomoditiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Komoditi;
+use Illuminate\Http\Request;
+
+class KomoditiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Komoditi  $komoditi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Komoditi $komoditi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Komoditi  $komoditi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Komoditi $komoditi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Komoditi  $komoditi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Komoditi $komoditi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Komoditi  $komoditi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Komoditi $komoditi)
+    {
+        //
+    }
+}
